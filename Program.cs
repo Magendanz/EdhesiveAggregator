e5b1c506fafcb28278978b8d8a935fba184e0967
Chad Magendanz <chad@magendanz.com>
2021-04-19 16:02:14 -0700

    Added new aggregation filters for labs and FRQs

--- Program.cs
+++ Program.cs
@@ -23,9 +23,17 @@ namespace EdhesiveAggregator
 
             dt.DeleteColumns(new string[] { "ID", "SIS User ID", "SIS Login ID", "Section" });
             RemoveEmptyColumns(dt);
-            AggregateColumns(dt, "Fast Start");
-            AggregateColumns(dt, "Review Questions");
-            AggregateColumns(dt, "Coding Activity");
+            AggregateColumns(dt, LessonFilter("Fast Start"));
+            AggregateColumns(dt, LessonFilter("Review Questions"));
+            AggregateColumns(dt, LessonFilter("Coding Activity"));
+            AggregateColumns(dt, LabFilter("Consumer Review"));
+            AggregateColumns(dt, LabFilter("Data"));
+            AggregateColumns(dt, LabFilter("Steganography"));
+            AggregateColumns(dt, LabFilter("Celebrity"));
+            AggregateColumns(dt, LabFilter("Magpie"));
+            AggregateColumns(dt, LabFilter("Elevens"));
+            AggregateColumns(dt, LabFilter("Picture"));
+            AggregateColumns(dt, MultipartFilter("FRQ"));
 
             // Save result and open with Excel
             dt.SaveCSV(@$"Output\{filename}");
@@ -49,15 +57,14 @@ namespace EdhesiveAggregator
             dt.DeleteColumns(names.ToArray());
         }
 
-        static void AggregateColumns(MutableDataTable dt, string filter)
+        static void AggregateColumns(MutableDataTable dt, string pattern)
         {
-            var pattern = @$"(Unit \d+: )Lesson \d.+?- {filter}.+";
             var input = string.Join('\n', dt.ColumnNames.ToArray());
             var matches = Regex.Matches(input, pattern);
             var names = new List<string>();
             foreach (Match match in matches)
             {
-                var name = match.Groups[1].Value + filter;
+                var name = match.Groups[1].Value + match.Groups[2].Value;
                 var col = dt.GetColumn(match.Value);
                 var target = dt.GetColumn(name);
                 if (target == null)
@@ -76,5 +83,12 @@ namespace EdhesiveAggregator
             }
             dt.DeleteColumns(names.ToArray());
         }
+
+        static string LessonFilter(string filter)
+            => @$"(Unit \d+: )Lesson \d.+?- ({filter}).+";
+        static string LabFilter(string filter)
+            => @$"({filter}): Activity [1-9] \(\d+\)";
+        static string MultipartFilter(string filter)
+            => @$"({filter}: )([\w\s]+) Part [A-Z] \(\d+\)";
     }
 }
